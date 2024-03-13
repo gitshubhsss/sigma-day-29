e44a44bdc13672ecbffa95e6acf7ee7772b094bc
gitshubhsss <shubhamranjane16@gmail.com>
2024-03-13 10:57:50 +0530

    pop will simply delete the last element of the array

---
+++ pop().js
@@ -0,0 +1,8 @@
+//pop will simply delete the last element that is present in the array 
+
+let cars=["swift","lamborgini","creata","kia"];
+let deletedCar=cars.pop();
+console.log(deletedCar);
+console.log(cars);
+//kia
+// [ 'swift', 'lamborgini', 'creata' ]
