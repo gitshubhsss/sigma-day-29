dbe06a9f1f2e632cfcc728c3442b5e5c1256b8b1
gitshubhsss <shubhamranjane16@gmail.com>
2024-03-13 11:51:25 +0530

    slice in array

---
+++ sliceInArray.js
@@ -0,0 +1,4 @@
+let studentNames=["shubham","pavan","sumit","ashish","omkar"];
+console.log(studentNames.slice(1));//[ 'pavan', 'sumit', 'ashish', 'omkar' ]
+console.log(studentNames.slice(2,3));//[ 'sumit' ] it will consder as last index -1 ie 3-1 ie 2
+console.log(studentNames.slice(-2));//[ 'ashish', 'omkar' ]
