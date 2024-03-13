d48d37ba522f15ae85b035c98a0842ff490548df
gitshubhsss <shubhamranjane16@gmail.com>
2024-03-13 12:19:22 +0530

    use of sort method in Js will only use this on the strings will not use it for the numbers cause it might not give the appropriate answer

---
+++ sort().js
@@ -0,0 +1,5 @@
+//use of sort() in Arrays
+let friends = ["shubham", "pavan", "ashish", "omkar", "sumit", "akash"];
+friends.sort();
+console.log(friends);
+//[ 'akash', 'ashish', 'omkar', 'pavan', 'shubham', 'sumit' ]
