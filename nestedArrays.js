a45b251b7df87c970f57633e8d01c2f17f8a92b9
gitshubhsss <shubhamranjane16@gmail.com>
2024-03-13 13:14:09 +0530

    tic tak toe game using nested Arrays

---
+++ nestedArrays.js
@@ -0,0 +1,7 @@
+//make a tic tak toe array using JavaScript nested arrys
+
+let tictack=[["x",null,"O"],[null,"X",null],["O",null,"X"]]
+console.log(tictack);
+tictack=[1][1]="0";
+console.log(tictack);
+console.log(tictack);
