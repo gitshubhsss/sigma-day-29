cf2896711cc87f383040b00853a9342ba55ab628
gitshubhsss <shubhamranjane16@gmail.com>
2024-03-13 12:26:59 +0530

    practiceQ5.js

---
+++ practiceQ5.js
@@ -0,0 +1,6 @@
+let langueages=["c","c++","HTML","JavaScript","python","java","c#","Sql"];
+langueages.reverse();
+console.log(langueages);//[ 'Sql', 'c#', 'java', 'python', 'JavaScript', 'HTML', 'c++', 'c' ]
+let indexofJS=langueages.indexOf("JavaScript");
+console.log(indexofJS);
+//4
