4834e6c513c48943b4846278be0681a7c5427e56
gitshubhsss <shubhamranjane16@gmail.com>
2024-03-13 10:53:44 +0530

    push will add the element at the last index of the array

---
+++ push().js
@@ -0,0 +1,5 @@
+//push - add at the end doesont care about the lenght
+
+let cars=["swift","lamborgini","creata","kia"];
+cars.push("Ennova");
+console.log(cars); //[ 'swift', 'lamborgini', 'creata', 'kia', 'Ennova' ]
